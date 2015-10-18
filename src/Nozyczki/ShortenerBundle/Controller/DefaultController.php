9fddeb553bbd5d7b229259d771f69af38132c75c
Tomasz Szymanek <tszymanek@o2.pl>
2015-10-18 21:47:29 +0200

    Wylistowanie linków

--- src/Nozyczki/ShortenerBundle/Controller/DefaultController.php
+++ src/Nozyczki/ShortenerBundle/Controller/DefaultController.php
@@ -2,18 +2,41 @@
 
 namespace Nozyczki\ShortenerBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Nozyczki\ShortenerBundle\Document\Link;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * @Route("/link")
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        $links = $this->get('doctrine_mongodb')->getRepository('NozyczkiShortenerBundle:Link')->findAll();
+
+        if (!$links) {
+            throw $this->createNotFoundException('No links found.');
+        }
+        return $this->render('default/index.html.twig', array(
+            'links' => $links
+        ));
+    }
+
+    /**
+     * @Route("/link/create")
+     */
+    public function createAction()
+    {
+        $link = new Link();
+        $link->setUrl('http://www.wp.pl');
+
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $dm->persist($link);
+        $dm->flush();
+
+        return new Response('Created link id '.$link->getId());
     }
 }
